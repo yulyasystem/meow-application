9f04330a9b6d8f90f4a8e52b98ccdc101a2b19c8
Юлия Бекенева <cactusyou@MacBook-Pro-Ulia.local>
2020-03-15 20:29:20 +0200

    my first meow app

---
+++ src/Happiness.svelte
@@ -0,0 +1,82 @@
+<script>
+  export let ease, slide;
+  let packOfLove = [];
+  let unpuckedLove = [
+    "meoew",
+    "you're beautiful",
+    "🌵 loves you",
+    "🧡🧡🧡🧡🧡🧡🧡🧡🧡",
+    "🦁 Tihon loves you",
+    "🐈 Velik loves you",
+    "You're awesome 💌",
+    "🌵🌵🌵🌵🌵🌵",
+    "^_^ meeeeeeeow",
+    "don't worry, COVID-19 will be cured",
+    "you're rock! ⛏️",
+    "🌵 loves you",
+    "🌵 loves you",
+    "🌵 loves you"
+  ];
+  let amountOfLove = 0;
+  let isEnough = false;
+
+  function showLove() {
+    amountOfLove++;
+    console.log(amountOfLove);
+
+    if (amountOfLove < 10) {
+      let randomItem =
+        unpuckedLove[Math.floor(Math.random() * unpuckedLove.length)];
+      packOfLove = [...packOfLove, randomItem];
+    } else isEnough = true;
+  }
+</script>
+
+<style>
+  .meow-section {
+    font-size: 24px;
+  }
+  .piece {
+    margin-bottom: 5px;
+  }
+
+  .big {
+    font-size: 34px;
+    transform: scale(1);
+    animation: pulse 3s infinite;
+  }
+
+  @keyframes pulse {
+    0% {
+      transform: scale(0.95);
+    }
+
+    70% {
+      transform: scale(1);
+    }
+
+    100% {
+      transform: scale(0.95);
+    }
+  }
+</style>
+
+<button
+  transition:slide={{ delay: 350, duration: 1100, easing: ease }}
+  on:click={showLove}>
+  Press to be happy
+</button>
+
+<div class="meow-section">
+  {#each packOfLove as pieceOfLove}
+    <div class="piece">{pieceOfLove}</div>
+  {/each}
+
+  {#if isEnough}
+    <p
+      class="big"
+      transition:slide={{ delay: 350, duration: 1100, easing: ease }}>
+      Got enough love, lets share! 💚 🧡 ❤️ 💜
+    </p>
+  {/if}
+</div>
---
+++ src/Header.svelte
@@ -0,0 +1,54 @@
+<script>
+  import { fade } from "svelte/transition";
+  import { quintOut } from "svelte/easing";
+  import { slide } from "svelte/transition";
+  import Happiness from "./Happiness.svelte";
+
+  let name = "";
+  let isShown = false;
+  let isEmpty = true;
+  function handleName(event) {
+    let enteredValue = event.target.value;
+    if (enteredValue.length !== 0) {
+      isEmpty = false;
+    } else isEmpty = true;
+  }
+
+  function showGreeting() {
+    isShown = true;
+  }
+</script>
+
+<style>
+  .form {
+    display: flex;
+    flex-direction: column;
+    text-align: left;
+    margin: 0 auto;
+    width: 300px;
+  }
+
+  label {
+    margin-bottom: 5px;
+  }
+</style>
+
+<header>
+  {#if !isShown}
+    <div transition:fade={{ delay: 100, duration: 100 }} class="form">
+      <label>What is your name?</label>
+      <div class="submit">
+        <input type="text" bind:value={name} on:input={handleName} />
+        <button on:click={showGreeting} disabled={isEmpty}>Okay</button>
+      </div>
+
+    </div>
+  {/if}
+
+  {#if isShown}
+    <h1 transition:slide={{ delay: 150, duration: 1500, easing: quintOut }}>
+      Hello, {name}!
+    </h1>
+    <Happiness {slide} ease={quintOut} />
+  {/if}
+</header>
